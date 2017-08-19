5a3c4747aaac3d41951d8516191931958490cf88
Jack Le Hamster <jacklehamster@gmail.com>
2017-08-19 14:41:17 -0700

    Some fixes

--- dobuki.net/public/sw.js
+++ dobuki.net/public/sw.js
@@ -1,27 +1,33 @@
 var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-    '',
-    'login.js',
-    'registerworker.js',
-    'style.css',
-    'scripts/main.js',
-    'scripts/solveworker.js',
-    'scripts/sudoku.js',
-    'scripts/sudoku.wasm',
-    'scripts/sudokucsolver.js',
-    'scripts/sudokusolver.js',
-    '//cdn.jsdelivr.net/react/15.5.4/react.min.js',
-    '//cdn.jsdelivr.net/react/15.5.4/react-dom.min.js',
-    '//code.jquery.com/jquery-3.2.1.min.js',
-    '//cdnjs.cloudflare.com/ajax/libs/blueimp-md5/2.8.0/js/md5.js',
-];
 
 self.addEventListener('install', function(event) {
     // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function(cache) {
-                return cache.addAll(urlsToCache,  {credentials: 'same-origin', redirect: 'follow'});
+                var urlsToPrefetch = [
+                    '',
+                    'login.js',
+                    'registerworker.js',
+                    'style.css',
+                    'scripts/main.js',
+                    'scripts/solveworker.js',
+                    'scripts/sudoku.js',
+                    'scripts/sudoku.wasm',
+                    'scripts/sudokucsolver.js',
+                    'scripts/sudokusolver.js',
+                    'https://cdn.jsdelivr.net/react/15.5.4/react.min.js',
+                    'https://cdn.jsdelivr.net/react/15.5.4/react-dom.min.js',
+                    'https://code.jquery.com/jquery-3.2.1.min.js',
+                    'https://cdnjs.cloudflare.com/ajax/libs/blueimp-md5/2.8.0/js/md5.js',
+                    'https://fonts.googleapis.com/css?family=Concert+One|Fredoka+One',
+                ];
+                return cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
+                    return new Request(urlToPrefetch, {mode: 'cors'});
+                }),  {redirect: 'follow'});
+            })
+            .catch(function(error) {
+                console.error('Pre-fetching failed:', error);
             })
     );
 });
@@ -50,12 +56,14 @@ self.addEventListener('fetch', function(event) {
                     }
                 );
 
-                if (response.redirected) {
-                    return Response.redirect(response.url);
-                }
-
                 // Cache hit - return response
                 if (response) {
+                    if (response.redirected) {
+                        for(var i in response) {
+                            console.log(i,response[i]);
+                        }
+                        return Response.redirect(response.url);
+                    }
                     return response;
                 }
 
